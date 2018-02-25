c5bf4a6272c7315899103df5ba3adb294fd37171
mntik <mntika@citycollege.sheffield.eu>
2018-02-25 23:23:34 +0200

    Create OrderProgram Project

---
+++ OrderProgram/src/OrderProgram.java
@@ -0,0 +1,20 @@
+
+public class OrderProgram {
+
+	public static void main(String[] args) {
+		Order o =  new Order();
+		
+		Product p1 = new Product("potatoes", 2.50);
+		Product p2 = new Product("spaghetti", 5.00);
+		Product p3 = new Product("hamburger", 4.20);
+		Product p4 = new Product("ceasar's", 4.80);
+		
+		o.add(p1, 3);
+		o.add(p2, 2);
+		o.add(p3, 2);
+		o.add(p4, 5);
+
+		o.printReceipt();
+	}
+
+}
