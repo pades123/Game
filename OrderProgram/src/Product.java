4662fee6157dd6a281d6b8661267d657e381e375
pades123 <pnikolaidis@citycollege.sheffield.eu>
2018-02-26 15:19:21 +0200

    Added product.java

---
+++ OrderProgram/src/Product.java
@@ -0,0 +1,14 @@
+
+public class Product {
+	private String name;
+	private double price;
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+}
